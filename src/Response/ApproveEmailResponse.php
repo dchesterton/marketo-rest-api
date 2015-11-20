9a54ebbbf8eebbe476cb8636f6079cce420faae7
Anthony P Tetlow <anthony.tetlow@thedrum.com>
2015-11-20 15:14:59 +0000

    Added functions for approve email, update email content and update email content in editable section

---
+++ src/Response/ApproveEmailResponse.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of the Marketo REST API Client package.
+ *
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CSD\Marketo\Response;
+
+use CSD\Marketo\Response;
+
+/**
+ * Response for the create/update lead API methods.
+ *
+ */
+class ApproveEmailResponse extends Response
+{
+
+}
---
+++ src/Response/UpdateEmailContentInEditableSectionResponse.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * This file is part of the Marketo REST API Client package.
+ *
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CSD\Marketo\Response;
+
+use CSD\Marketo\Response;
+
+/**
+ * Response for the email content in editable section API method.
+ *
+ */
+class UpdateEmailContentInEditableSectionResponse extends Response
+{
+
+}
